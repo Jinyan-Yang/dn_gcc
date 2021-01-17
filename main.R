1793529113894be08779993c977e907fc4f0a26d
JinyanYang <jyyoung@live.com>
2021-01-17 12:16:45 +1100

    with comments

--- main.R
+++ main.R
@@ -3,10 +3,10 @@ library(jpeg)
 # library(gridExtra)
 library(exifr)
 library(doBy)
-# get ng####
+
 source('r/function_get_gcc.R')
 
-# 
+# calculate GCC for site
 cal.gcc.site.func('qp',ROI = c(.2,.8,.2,.8))
 cal.gcc.site.func('ng',ROI = c(.2,.8,.2,.8))
 cal.gcc.site.func('mp',ROI = c(.2,.8,.2,.8))
